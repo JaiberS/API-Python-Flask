454e289bb4902e995e8fc54f55cdc46fff220128
Jaiber Ramirez <jramirez@masiv.com>
2024-07-07 14:48:07 -0500

    added authentication to the api

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, jsonify, request, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+USERNAME = os.getenv('API_USER', 'default_user')
+PASSWORD = os.getenv('API_PASSWORD', 'default_password')
+
+@auth.verify_password
+def verify_password(username, password):
+    if username == USERNAME and password == PASSWORD:
+        return username
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,16 +37,19 @@ def home():
 
 
 @app.route('/users', methods=['GET'])
+@auth.login_required
 def get_users():
     users = User.query.all()
     return jsonify([{'id': user.id, 'name': user.name} for user in users])
 
 @app.route('/users/<int:user_id>', methods=['GET'])
+@auth.login_required
 def get_user(user_id):
     user = User.query.get_or_404(user_id)
     return jsonify({'id': user.id, 'name': user.name})
 
 @app.route('/users', methods=['POST'])
+@auth.login_required
 def create_user():
     if not request.json or not 'name' in request.json:
         abort(400)
@@ -42,6 +59,7 @@ def create_user():
     return jsonify({'id': user.id, 'name': user.name}), 201
 
 @app.route('/users/<int:user_id>', methods=['PUT'])
+@auth.login_required
 def update_user(user_id):
     user = User.query.get_or_404(user_id)
     user.name = request.json.get('name', user.name)
@@ -49,6 +67,7 @@ def update_user(user_id):
     return jsonify({'id': user.id, 'name': user.name})
 
 @app.route('/users/<int:user_id>', methods=['DELETE'])
+@auth.login_required
 def delete_user(user_id):
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
